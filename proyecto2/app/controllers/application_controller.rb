14694bd4c68734746aa01f730f3c2d492434822d
octavionancul <octavionancul@gmail.com>
2018-03-03 00:26:42 -0300

    autentificacion

--- proyecto2/app/controllers/application_controller.rb
+++ proyecto2/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   protect_from_forgery with: :exception
+
+protected
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :name])
+
+		devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :name])
+	end
 end
--- proyecto2/config/routes.rb
+++ proyecto2/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   resources :products
   get 'products/add'
 
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
   get 'pages/index'
 
   get 'pages/about'
+root 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
